295bb38833aab1526df9c0a673f3c11115b968c2
creeper12356 <creeperhjt@sjtu.edu.cn>
2023-09-07 11:23:51 +0800

    modify src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 
-    QDir::setCurrent("..");
+    // QDir::setCurrent("..");
     QApplication a(argc, argv);
 //    QGraphicsScene scene;
 //    QGraphicsView view;
