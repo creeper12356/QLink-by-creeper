132400f8eee99211d715e10fc682930ce5ff29c4
creeper12356 <creeperhjt@sjtu.edu.cn>
2023-10-11 14:56:52 +0800

    update

--- main.cpp
+++ main.cpp
@@ -11,15 +11,9 @@ int main(int argc, char *argv[])
      QDir::setCurrent("..");
 
     QApplication a(argc, argv);
-//    QGraphicsScene scene;
-//    QGraphicsView view;
 
     WelcomePage wp;
-//    scene.addWidget(&wp);
-//    view.setScene(&scene);
-//    view.resize(wp.size());
-//    view.setForegroundBrush(QColor(0,0,150,100));
-//    view.show();
+
     wp.show();
     return a.exec();
 }
